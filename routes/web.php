dc974f7d5589fdcdc8edb786475cb16faea4bb8b
佐藤洋一 <sarub@Yoichi.local>
2022-09-05 19:18:52 +0900

    “管理者ユーザが作成できない”

--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,11 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'WebController@index');
 
 Route::get('users/carts', 'CartController@index')->name('carts.index');
-
 Route::post('users/carts', 'CartController@store')->name('carts.store');
-
+Route::put('users/carts', 'CartController@update')->name('carts.update');
 Route::delete('users/carts', 'CartController@destroy')->name('carts.destroy');
 
 Route::get('users/mypage', 'UserController@mypage')->name('mypage');
@@ -28,13 +25,24 @@ Route::put('users/mypage', 'UserController@update')->name('mypage.update');
 Route::get('users/mypage/favorite', 'UserController@favorite')->name('mypage.favorite');
 Route::get('users/mypage/password/edit', 'UserController@edit_password')->name('mypage.edit_password');
 Route::put('users/mypage/password', 'UserController@update_password')->name('mypage.update_password');
+Route::delete('users/mypage/delete', 'UserController@destroy')->name('mypage.destroy');
 
 Route::post('products/{product}/reviews', 'ReviewController@store');
 
 Route::get('products/{product}/favorite', 'ProductController@favorite')->name('products.favorite');
-
-Route::resource('products', 'ProductController');
-
+Route::get('products', 'ProductController@index')->name('products.index');
+Route::get('products/{product}', 'ProductController@show')->name('products.show');
 Auth::routes(['verify' => true]);
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/dashboard', 'DashboardController@index')->middleware('auth:admins');
+
+Route::group(['prefix' => 'dashboard', 'as' => 'dashboard.'], function () {
+    Route::get('login', 'Dashboard\Auth\LoginController@showLoginForm')->name('login');
+    Route::post('login', 'Dashboard\Auth\LoginController@login')->name('login');
+});
+
+if (env('APP_ENV') === 'production') {
+    URL::forceScheme('https');
+}
